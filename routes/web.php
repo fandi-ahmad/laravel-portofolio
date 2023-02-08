80a49597c6e7569d3bad8f9d94dfd448a6513259
fandi-ahmad <fandi4160@gmail.com>
2023-02-08 15:51:06 +0800

    make routes for page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\authController;
+use App\Http\Controllers\pageController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,13 @@ Route::get('/auth/redirect', [authController::class, "redirect"])->middleware('g
 Route::get('/auth/callback', [authController::class, "callback"])->middleware('guest');
 Route::get('/auth/logout', [authController::class, "logout"]);
 
-Route::get('/dashboard', function() {
-    return 'Selamat datang ' . Auth::user()->name . ' di halaman dashboard';
-})->middleware('auth');
+
+Route::prefix('dashboard')->middleware('auth')->group(
+    function() {
+        Route::get('/', function() {
+            return view('dashboard.layout');
+        });
+        Route::resource('page', pageController::class);
+        // Route::get('page', [pageController::class, "index"]);
+    }
+);
